1e4682229a5440b4f161c66c4dd7f16d8b8e6eaa
Kaio <128155244+Kaio0809@users.noreply.github.com>
2023-04-06 14:38:40 +0000

    exericio concluído

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ a = int(input('Digite um número inteiro: '))
 b = int(input('Digite outro número inteiro: '))
 c = int(input('Digite um terceiro número inteiro: '))
 
-ab = b-a
-bc = c-b
-ca = a-c
-
 if (a + b > c) and (a + c > b) and (b + c > a):
-    if ab == bc == ca: 
+    if a == b == c: 
         print('Equilátero')
     else:
-        if (ab == bc) or (bc == ca) or (ca == ab):
+        if (a == b) or (b == c) or (c == a):
             print('Isósceles')
-        elif (ab != bc) and (bc != ca) and (ca != ab):
+        elif (a != b) and (b != c) and (c != a):
             print('Escaleno')
 else:
     print('Não é um triângulo')
